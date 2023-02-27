51f0d194485d946a698d85a8aab35b4a9bd9e14c
javiersalinas00 <javiersalinas00@gmail.com>
2023-02-27 18:16:19 -0300

    Version 1.0

---
+++ juego piedra papel o tijera.js
@@ -0,0 +1,23 @@
+var myArray = ['Piedra', 'Papel', 'Tijera'];
+var computador = myArray[~~(Math.random() * myArray.length)];
+//console.log(computador);
+var opcionJugador = prompt('Elije: Piedra papel o Tijera!');
+if (computador === opcionJugador) {
+    console.log("Empate!");
+}if ( computador == 'Piedra' && opcionJugador == 'Tijera' ) {
+    console.log("Ganó Piedra");
+}if ( computador == 'Piedra' && opcionJugador == 'Papel' ) {
+    console.log("Ganó Piedra");
+}if ( computador == 'Papel' && opcionJugador == 'Piedra' ) {
+    console.log("Ganó Piedra");
+}if ( computador == 'Papel' && opcionJugador == 'Tijera' ) {
+    console.log("Gano Tijera");
+}if ( computador == 'Papel' && opcionJugador == 'Piedra' ) {
+    console.log("Ganó Piedra");
+}if ( computador == 'Tijera' && opcionJugador == 'Papel' ) {
+    console.log("Ganó Tijera");
+}if ( computador == ' Tijera' && opcionJugador == 'Piedra' ) {
+    console.log("Ganó Piedra");
+}if ( computador == opcionJugador ) {
+    console.log("Empate!");
+};
